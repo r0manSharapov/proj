b575f7794a54c60e191f04480b2d90512aa4cd12
martantunes <masu131120000@gmail.com>
2020-05-06 15:34:22 +0100

    Class User alterações

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -16,6 +16,8 @@ class RedirectIfAuthenticated
      * @param  string|null  $guard
      * @return mixed
      */
+
+
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
