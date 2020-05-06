52263ea0c929d244e9127aa5ff64c5f9f7f06ac1
martantunes <masu131120000@gmail.com>
2020-05-06 15:55:50 +0100

    web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
 
 Auth::routes(['verify' => true]);
 
