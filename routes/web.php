e36340b78a5fffe2142698ae3f613017bfe6da81
r0manSharapov <roman252sharapov252@gmail.com>
2020-05-06 16:07:08 +0100

    env modificated mailtrap no encryption, registation varification

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware("verified");
