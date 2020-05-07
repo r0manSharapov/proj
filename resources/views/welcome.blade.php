b0a1050edac09b734693b74e7151b54b3d319504
martantunes <masu131120000@gmail.com>
2020-05-07 14:41:55 +0100

    melhorado design v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,30 +5,32 @@
 
 
                         <h1 class="cover-heading" style="margin-top: 50px">Finanças Pessoais</h1>
-                        <p class="lead" style="margin-left: 100px; margin-bottom: 100px; margin-right: 100px;">
+                        <p class="lead" style="margin-left: 100px; margin-bottom: 50px; margin-right: 100px;">
                             A aplicação web "Finanças Pessoais" tem como principal objetivo ajudar os utilizadores a gerir as suas finanças pessoais.
                             Os utilizadores têm uma área privada, onde poderão registar todos os seus movimentos financeiros (receitas e despesas) organizados por contas, ver um sumário do estado das suas finanças e aceder a informação estatística sobre as suas receitas e despesas.
                         </p>
 
-                        
+
 
 
                         <div class ="container">
 
-                <h2>Estatísticas</h2>
 
 
-                <div >
 
-                    <div >Total Utilizadores Registados
+
+
+                            <h4>Total Utilizadores Registados </h4>
                     <p>{{$totalUtilizadores ?? 0}}</p>
-                    </div>
-                    <div >Total de Contas
+
+                    <h4>Total de Contas</h4>
                     <p>{{$totalContas ?? 0}}</p>
-                    </div>
-                    <div >Total de Movimentos
+
+                    <h4 >Total de Movimentos </h4>
                     <p>{{$totalMovimentos ?? 0}}</p>
-                    </div>
+
+
+
                 </div>
                         </div>
 
