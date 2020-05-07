fe53cc2ba4ca931cef4126fcebf68f0543ca5782
martantunes <masu131120000@gmail.com>
2020-05-07 14:37:05 +0100

    melhorado design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,38 @@
 @extends('layouts.app')
 @section('content')
-            <div class="content">
-                <div class="title m-b-md">
-                    Finanças Pessoais
-                </div>
-                <h2>Apresentação</h2>
-                <p>
-                    A aplicação web "Finanças Pessoais" tem como principal objetivo ajudar os utilizadores a gerir as suas finanças pessoais.
-                    Os utilizadores têm uma área privada, onde poderão registar todos os seus movimentos financeiros (receitas e despesas) organizados por contas, ver um sumário do estado das suas finanças e aceder a informação estatística sobre as suas receitas e despesas.
-                </p>
+
+            <div class="text-center">
+
+
+                        <h1 class="cover-heading" style="margin-top: 50px">Finanças Pessoais</h1>
+                        <p class="lead" style="margin-left: 100px; margin-bottom: 100px; margin-right: 100px;">
+                            A aplicação web "Finanças Pessoais" tem como principal objetivo ajudar os utilizadores a gerir as suas finanças pessoais.
+                            Os utilizadores têm uma área privada, onde poderão registar todos os seus movimentos financeiros (receitas e despesas) organizados por contas, ver um sumário do estado das suas finanças e aceder a informação estatística sobre as suas receitas e despesas.
+                        </p>
+
+                        
+
+
+                        <div class ="container">
+
                 <h2>Estatísticas</h2>
 
 
-                <div class="estatisticas">
+                <div >
 
-                    <div class="p_estatisticas">Total Utilizadores Registados
+                    <div >Total Utilizadores Registados
                     <p>{{$totalUtilizadores ?? 0}}</p>
                     </div>
-                    <div class="p_estatisticas">Total de Contas
+                    <div >Total de Contas
                     <p>{{$totalContas ?? 0}}</p>
                     </div>
-                    <div class="p_estatisticas">Total de Movimentos
+                    <div >Total de Movimentos
                     <p>{{$totalMovimentos ?? 0}}</p>
                     </div>
                 </div>
-            </div>
+                        </div>
+
+
+
+    </div>
 @endsection
