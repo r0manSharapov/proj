7655f24fe6cb2c6ebf48908c11be6c1387f9079f
martantunes <masu131120000@gmail.com>
2020-05-15 18:10:07 +0100

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,9 +76,14 @@
                                     <a class="dropdown-item" href="{{ route('settings') }}">
                                         {{ __('User Settings') }}
                                     </a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
 
                                 </div>
                             </li>
