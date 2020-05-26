8d2168d766fd66510774d62db490b77d3e008f08
Andreia Pereira <alexandraper12@hotmail.com>
2020-05-26 23:12:29 +0100

    erro na users list check

--- resources/views/usersList.blade.php
+++ resources/views/usersList.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title','Users List)
+@section('title','Users List')
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
